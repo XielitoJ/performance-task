12e504bc8e247f8d9db91ec06c56db96865485d9
Pebble <156123932+XielitoJ@users.noreply.github.com>
2024-02-26 13:07:32 -0600

    Start of game, character controls

---
+++ images.g.ts
@@ -0,0 +1,114 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "cat":return img`
+. f f . . . . . f f . . . . . . 
+. f b f . . . f e f . . . . . . 
+. f b b f f f e e f . . . . . . 
+f b b b 1 1 e e e e f . . f f . 
+f b b 1 1 1 1 e e e f . . f 1 f 
+f 1 f 1 1 1 f 1 1 1 f . . f e f 
+f 1 1 1 1 1 1 1 1 1 f . f 1 e f 
+. f 1 1 1 1 1 1 1 f f f b 1 f . 
+. . f f f f f f f 1 e e b f . . 
+. . f e 1 1 b b b 1 1 1 f . . . 
+. . f e 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 1 1 1 f . . . 
+. . f b f b f f f b f b f . . . 
+. . . f . f . . . f . f . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage":return img`
+.........................
+.........................
+..............f.....f....
+.............fef...f1f...
+............febefff1bf...
+.......fff..fee111eeef...
+...ff.f11efff11ee11eef...
+..f11fe11eef11eef111fef..
+.feffee111ee11eef111f1f..
+..f..fee111e111e11b111f..
+......fe111ee11111111f...
+......feeeeeee111111f....
+.......fffffffffffff.....
+.........................
+.........................
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "catimation":
+            case "fraAzy}pX8-OYs%t:SGR":return [img`
+. f f . . . . . f f . . . . . . 
+. f b f . . . f e f . . . . . . 
+. f b b f f f e e f . . . . . . 
+f b b b 1 1 e e e e f . . f f . 
+f b b 1 1 1 1 e e e f . . f 1 f 
+f 1 f 1 1 1 f 1 1 1 f . . f e f 
+f 1 1 1 1 1 1 1 1 1 f . f 1 e f 
+. f 1 1 1 1 1 1 1 f f f b 1 f . 
+. . f f f f f f f 1 e e b f . . 
+. . f e 1 1 b b b 1 1 1 f . . . 
+. . f e 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 1 1 1 f . . . 
+. f b f f b f f f b f f b f . . 
+. . f . . f . . . f . . f . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. f f . . . . . f f . . . . . . 
+. f b f . . . f e f . . . . . . 
+. f b b f f f e e f . . . . . . 
+f b b b 1 1 e e e e f . . f f . 
+f b b 1 1 1 1 e e e f . . f 1 f 
+f 1 f 1 1 1 f 1 1 1 f . . f e f 
+f 1 1 1 1 1 1 1 1 1 f . f 1 e f 
+. f 1 1 1 1 1 1 1 f f f b 1 f . 
+. . f f f f f f f 1 e e b f . . 
+. . f e 1 1 b b b 1 1 1 f . . . 
+. . f e 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 1 1 1 1 f . . 
+. . f b f f b f f f b f b f . . 
+. . . f . . f . . . f . f . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. f f . . . . . f f . . . . . . 
+. f b f . . . f e f . . . . . . 
+. f b b f f f e e f . . . . . . 
+f b b b 1 1 e e e e f . . f f . 
+f b b 1 1 1 1 e e e f . . f 1 f 
+f 1 f 1 1 1 f 1 1 1 f . . f e f 
+f 1 1 1 1 1 1 1 1 1 f . f 1 e f 
+. f 1 1 1 1 1 1 1 f f f b 1 f . 
+. . f f f f f f f 1 e e b f . . 
+. . f e 1 1 b b b 1 1 1 f . . . 
+. . f e 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 1 1 1 f . . . 
+. . f b f b f f f b f b f . . . 
+. . . f . f . . . f . f . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
